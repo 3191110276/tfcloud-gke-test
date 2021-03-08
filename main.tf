82017a482a1a42a4375a659f6bbf7343bfb0dcc1
Michael Maurer <3191110276@users.noreply.github.com>
2021-03-08 10:41:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,18 +3,10 @@ resource "local_file" "key" {
     filename = "${path.module}/key.json"
 }
 
-resource "google_service_account" "default" {
-  account_id   = var.serviceaccountid
-  display_name = var.serviceaccountname
-}
-
 resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
   location = "us-central1"
 
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
 }
@@ -28,11 +20,5 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   node_config {
     preemptible  = true
     machine_type = "e2-micro"
-
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.default.email
-    oauth_scopes    = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
   }
 }
