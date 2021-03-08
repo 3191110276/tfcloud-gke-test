e4721cb7cbc037169f9a0592b84db433529fd426
Michael Maurer <3191110276@users.noreply.github.com>
2021-03-08 12:09:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,11 +20,11 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   node_config {
     preemptible  = true
     machine_type = "e2-micro"
+    
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
   }
-  
-  # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-  service_account = google_service_account.default.email
-  oauth_scopes    = [
-    "https://www.googleapis.com/auth/cloud-platform"
-  ]
 }
